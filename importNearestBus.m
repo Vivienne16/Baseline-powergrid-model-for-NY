12b9e844371cf65e49e61c006a0cce3054204799
Vivienne16 <39807165+Vivienne16@users.noreply.github.com>
2021-07-20 12:12:22 -0400

    Add files via upload

---
+++ importNearestBus.m
@@ -0,0 +1,37 @@
+function genAllocation = importNearestBus(filename, dataLines)
+%Import generator allocation
+%
+%  Example:
+%  nearestnypvbustogen = importNearestBus("D:\EERL\NY-Simple-Net\NY-Simple-Net-main\Data\nearest_bus_0601.csv", [2, Inf]);
+
+%% Input handling
+
+% If dataLines is not specified, define defaults
+if nargin < 2
+    dataLines = [2, Inf];
+end
+
+%% Setup the Import Options and import the data
+opts = delimitedTextImportOptions("NumVariables", 14);
+
+% Specify range and delimiter
+opts.DataLines = dataLines;
+opts.Delimiter = ",";
+
+% Specify column names and types
+opts.VariableNames = ["fid", "NYISOName","PTID","Zone","UnitType","FuelType",...
+    "Latitude","Longitude","BusName","BusDist"];
+opts.SelectedVariableNames = ["NYISOName", "PTID", "BusName", "BusDist"];
+opts.VariableTypes = ["string", "categorical", "categorical", "string","string","string",...
+    "string","string","categorical", "double"];
+
+% Specify file level properties
+opts.ExtraColumnsRule = "ignore";
+opts.EmptyLineRule = "read";
+
+% Specify variable properties
+
+% Import the data
+genAllocation = readtable(filename, opts);
+
+end
