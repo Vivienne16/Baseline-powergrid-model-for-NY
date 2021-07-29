cf65472ed2eff5ac519353ae66f4ec0011d5ec9d
Bo Yuan <by276@cornell.edu>
2021-07-28 22:26:07 -0400

    Edit modifyMPC: incorporate matpower recommended settings to improve readability

--- ModifyMPC.m
+++ ModifyMPC.m
@@ -1,61 +1,74 @@
+%% ModifyMPC
+% Read the original NPCC 140-bus MATPOWER case file, modify the bus and
+% branch matrices, and write the updated MATPOWER case file.
+
+% Notes:
+%%%% Consider reading the interface limits from a external file
+%%%% Why do you only set rate A-C for the NE lines?
+
+% Author: Vivienne Liu
+% Last modified: July 28, 2021
+
 close all;
 clear all;
-clc
+clc;
 
-%% MPC modification
+%% Load the original MATPOWER case
 % Load original NPCC-140 bus MATPOWER case
-load('Data/npcc.mat','mpc');
+mpc = loadcase('Data/npcc.mat');
 
-% Add MOST constant parameter names
+% Add MATPOWER constant parameter names
 define_constants;
 
-%% delete transmission lines between PJM and IESO
-mpc.branch(find((mpc.branch(:,1) == 84).*(mpc.branch(:,2)==116)==1),:)=[];
-mpc.branch(find((mpc.branch(:,1) == 87).*(mpc.branch(:,2)==115)==1),:)=[];
-mpc.branch(find((mpc.branch(:,1) == 90).*(mpc.branch(:,2)==114)==1),:)=[];
-
+%% Modify MATPOWER case
+%% Bus modification
 % fix negative load
-mpc.bus(:,3) = abs(mpc.bus(:,3));
+mpc.bus(:,PD) = abs(mpc.bus(:,PD));
 
-%% update branch limit external
-%PJM 124-75, 125-81, 134-66, 138-67
-mpc.branch(find((mpc.branch(:,1) == 66).*(mpc.branch(:,2)==134)==1),6) = 315;
-mpc.branch(find((mpc.branch(:,1) == 67).*(mpc.branch(:,2)==138)==1),6) = 660;
-mpc.branch(find((mpc.branch(:,1) == 81).*(mpc.branch(:,2)==125)==1),6) = 660;
-mpc.branch(find((mpc.branch(:,1) == 75).*(mpc.branch(:,2)==124)==1),6) = 2000;
-% mpc.branch(find((mpc.branch(:,1) == 60).*(mpc.branch(:,2)==140)==1),6) = 550;
-
-% 
-% NE 29-37 35-73
-mpc.branch(find((mpc.branch(:,1) == 29).*(mpc.branch(:,2)==37)==1),6:8) = 200;
-mpc.branch(find((mpc.branch(:,1) == 35).*(mpc.branch(:,2)==73)==1),6:8) = 1400;
-% 
-%IESO 100-48 102-54 103-54
-mpc.branch(find((mpc.branch(:,1) == 48).*(mpc.branch(:,2)==100)==1),6) = 450;
-mpc.branch(find((mpc.branch(:,1) == 54).*(mpc.branch(:,2)==102)==1),6) = 650;
-mpc.branch(find((mpc.branch(:,1) == 54).*(mpc.branch(:,2)==103)==1),6) = 650;
-
-%% Update bus types
+% Update bus types
 %PQ - PV
-mpc.bus(mpc.bus(:,1) == 39,2) = 2;
-mpc.bus(mpc.bus(:,1) == 77,2) = 2;
-mpc.bus(mpc.bus(:,1) == 45,2) = 2;
-mpc.bus(mpc.bus(:,1) == 62,2) = 2;
+mpc.bus(mpc.bus(:,BUS_I) == 39,BUS_TYPE) = PV;
+mpc.bus(mpc.bus(:,BUS_I) == 77,BUS_TYPE) = PV;
+mpc.bus(mpc.bus(:,BUS_I) == 45,BUS_TYPE) = PV;
+mpc.bus(mpc.bus(:,BUS_I) == 62,BUS_TYPE) = PV;
 
 %PV -PQ
-mpc.bus(mpc.bus(:,1) == 72,2) = 1;
-mpc.bus(mpc.bus(:,1) == 53,2) = 1;
-mpc.bus(mpc.bus(:,1) == 54,2) = 1;
-mpc.bus(mpc.bus(:,1) == 68,2) = 1;
+mpc.bus(mpc.bus(:,BUS_I) == 72,BUS_TYPE) = PQ;
+mpc.bus(mpc.bus(:,BUS_I) == 53,BUS_TYPE) = PQ;
+mpc.bus(mpc.bus(:,BUS_I) == 54,BUS_TYPE) = PQ;
+mpc.bus(mpc.bus(:,BUS_I) == 68,BUS_TYPE) = PQ;
 
 %slack
-mpc.bus(mpc.bus(:,1) == 78,2) = 1;
-mpc.bus(mpc.bus(:,1) == 74,2) = 3;
+mpc.bus(mpc.bus(:,BUS_I) == 78,BUS_TYPE) = PQ;
+mpc.bus(mpc.bus(:,BUS_I) == 74,BUS_TYPE) = REF;
 
-%% Add branch
-% add E-G 
-addline = [38 77 0.02 0.02 0 0 0 0 0 0 1 -360 360];
-mpc.branch = [mpc.branch;addline];
+%% Branch modification
+% delete transmission lines between PJM and IESO
+mpc.branch((mpc.branch(:,F_BUS) == 84)&(mpc.branch(:,T_BUS)==116),:)=[];
+mpc.branch((mpc.branch(:,F_BUS) == 87)&(mpc.branch(:,T_BUS)==115),:)=[];
+mpc.branch((mpc.branch(:,F_BUS) == 90)&(mpc.branch(:,T_BUS)==114),:)=[];
+
+% Update branch limit external
+% PJM 124-75, 125-81, 134-66, 138-67
+mpc.branch((mpc.branch(:,F_BUS) == 66)&(mpc.branch(:,T_BUS)==134),RATE_A) = 315;
+mpc.branch((mpc.branch(:,F_BUS) == 67)&(mpc.branch(:,T_BUS)==138),RATE_A) = 660;
+mpc.branch((mpc.branch(:,F_BUS) == 81)&(mpc.branch(:,T_BUS)==125),RATE_A) = 660;
+mpc.branch((mpc.branch(:,F_BUS) == 75)&(mpc.branch(:,T_BUS)==124),RATE_A) = 2000;
+mpc.branch((mpc.branch(:,F_BUS) == 60)&(mpc.branch(:,T_BUS)==140),RATE_A) = 550;
+
+% NE 29-37 35-73
+mpc.branch((mpc.branch(:,F_BUS) == 29)&(mpc.branch(:,T_BUS)==37),RATE_A:RATE_C) = 200;
+mpc.branch((mpc.branch(:,F_BUS) == 35)&(mpc.branch(:,T_BUS)==73),RATE_A:RATE_C) = 1400;
+
+% IESO 100-48 102-54 103-54
+mpc.branch((mpc.branch(:,F_BUS) == 48)&(mpc.branch(:,T_BUS)==100),RATE_A) = 450;
+mpc.branch((mpc.branch(:,F_BUS) == 54)&(mpc.branch(:,T_BUS)==102),RATE_A) = 650;
+mpc.branch((mpc.branch(:,F_BUS) == 54)&(mpc.branch(:,T_BUS)==103),RATE_A) = 650;
+
+% Add branch
+% Add E-G 
+newlineEG = [38 77 0.02 0.02 0 0 0 0 0 0 1 -360 360];
+mpc.branch = [mpc.branch;newlineEG];
 
 %% Save updated MATPOWER case
-save('Result/mpcupdated.mat','mpc')
+savecase('Result/mpcupdated.mat',mpc)
