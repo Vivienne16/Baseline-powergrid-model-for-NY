afbb2f9c648e079b0088703984e3a140550ba6ef
Bo Yuan <by276@cornell.edu>
2021-08-16 21:29:28 -0400

    add main function

---
+++ main.m
@@ -0,0 +1,21 @@
+%% Main file
+% Description of the main file
+%
+%
+
+%% Modify MPC
+modifyMPC
+
+
+%% Operation condition update
+OperationConditionUpdate
+
+
+%% PF test
+PFtestcase
+
+
+
+%% OPF test
+OPFtestcase
+
