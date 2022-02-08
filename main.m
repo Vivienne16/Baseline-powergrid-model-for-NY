286580d5d586a3a1d038e07fba5fb31e430b012f
Bo Yuan <by276@cornell.edu>
2022-02-08 17:10:39 -0500

    simplify main funciton structure; combine one hour and the loop

--- main.m
+++ main.m
@@ -1,32 +1,45 @@
 %% Main file
-% Baseline power grid model for NYS
-%
+%   Baseline power grid model for NYS
+%   Specify timestep(s) and runtime options and run the simulation.
 
 %   Created by Vivienne Liu
 %   Last modified on Feb. 8, 2022
 
-%% Parameter settings
-
-clear;
-clc;
-close all;
+clear; clc; close all;
 tic;
 
-savefig = true; % Save figure or not
-savedata = true; % Save PF and OPF results
-verbose = false; % Verbose printing or not
-runloop = false; % Loop through the whole year or not
-addrenew = true; % Add additional renewable or not
-usemat = true; % Read mat files
+%% Input parameters
 
+% Simulation timestep(s) for the model. 
+% Set it as an array if you want to run multiple timesteps in a loop.
+
+testyear = 2016;
+testmonth = 8;
+testday = 10;
+testhour = 11:12;
+
+%% Runtime options
+
+% Save figure or not
+savefig = true;
+% Save PF and OPF results or not
+savedata = true; 
+% Verbose printing or not
+verbose = false; 
+% Add additional renewable or not
+addrenew = true; 
+% Read mat files, otherwise read csv files
+usemat = true;
 % Add project to MATLAB path
 addpath(genpath("."))
 
 %% Data preparation
 
-% Specify a year, and download and format the data in that year. Downlaoded
+% Download and format the data in the specified year. Downlaoded
 % data are stored in the "Prep" directory. Formatted data are stored in the
-% "Data" directory.
+% "Data" directory. If the data has already been prepared, it will
+% automatically skip the process.
+%
 % Data sources include:
 %   1. NYISO: (1) hourly fuel mix
 %             (2) hourly interface flow
@@ -37,7 +50,6 @@ addpath(genpath("."))
 %   3. NRC: (1) Daily nuclear capacity factor
 %   4. EIA: (1) Monthly hydro generation data for Niagara and St. Lawrence
 
-testyear = 2016;
 writeFuelmix(testyear);
 writeInterflow(testyear);
 writePrice(testyear);
@@ -54,65 +66,28 @@ writeNuclearGen(testyear);
 
 mpc = modifyMPC();
 
-%% Test case for one hour
+%% Run the model
 
-% Specify a timestamp with year, month, day and hour, update operation
+% Run the model at the specified timestamp(s). Firstly, update operation
 % condition for that timestamp, and then run power flow (PF) and optimal
 % power flow (OPF). Produce figures for comparing simulation results with
 % historical data, including interface flow, fuel mix and locational
-% marginal price (LMP).
+% marginal price (LMP). The loop can run in parallel if a parallel pool is
+% provided on the machine.
 
-if runloop == false
-
-    testyear = 2016;
-    testmonth = 8;
-    testday = 10;
-    testhour = 12;
-    
-    timeStamp = datetime(testyear,testmonth,testday,testhour,0,0,"Format","MM/dd/uuuu HH:mm:ss");
-    fprintf("Start running %s ...\n",datestr(timeStamp));
- 
-    % Update operation conditions
-    mpcreduced = updateOpCond(mpc,timeStamp,savedata,verbose,usemat);
-    
-    % Add additional renewables if provided
-    if addrenew == true
-       mpcreduced = addRenewable(mpcreduced,timeStamp); 
-    end
-    
-    % Run power flow
-    resultPF = PFtestcase(mpcreduced,timeStamp,savefig,savedata,addrenew);
-    
-    % Run optimal power flow
-    resultOPF = OPFtestcase(mpcreduced,timeStamp,savefig,savedata,addrenew);
-end
-
-%% Loop through the whole year 2019
-
-% Specify the range of year, month, day, and hour that you want to loop
-% though, update operation condition for the specified timestamps, and then
-% run PF and OPF.
-
-if runloop == true
-    testyear = 2019;
-    for testmonth = 3
-        for testday = 10
-            parfor testhour = 1:3
-                
-                timeStamp = datetime(testyear,testmonth,testday,testhour,0,0,"Format","MM/dd/uuuu HH:mm:ss");
+for y = testyear
+    for m = testmonth
+        for d = testday
+            parfor h = testhour
+                timeStamp = datetime(y,m,d,h,0,0,"Format","MM/dd/uuuu HH:mm:ss");
                 fprintf("Start running %s ...\n",datestr(timeStamp));
-                
                 % Update operation conditions
                 mpcreduced = updateOpCond(mpc,timeStamp,savedata,verbose,usemat);
-                
                 % Run power flow
                 resultPF = PFtestcase(mpcreduced,timeStamp,savefig,savedata,addrenew);
-                
                 % Run optimal power flow
                 resultOPF = OPFtestcase(mpcreduced,timeStamp,savefig,savedata,addrenew);
-                
                 fprintf("Success!\n");
-                
             end
         end
     end
